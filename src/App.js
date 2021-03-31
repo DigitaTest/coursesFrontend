c840a4a469d2bf53e9122d2b298ead6c61d28007
Laza993 <laza.bogibilder223@gmail.com>
2021-03-31 22:07:30 +0200

    routing started

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './index.css'
 import Header from './components/header/'
 import Footer from './components/footer/'
 import Main from './components/main/'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Router>
+        <Header />
+        <Main />
+        <Footer />
+      </Router>
     </>
   )
 }
