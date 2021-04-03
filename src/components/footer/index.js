d07a4207e8e9ec47ad518324a990f5d719391367
Laza993 <laza.bogibilder223@gmail.com>
2021-04-03 23:49:42 +0200

    foter up

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,6 @@
 import './footer.css'
 import { Link } from 'react-router-dom'
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 const Index = () => {
   return (
@@ -13,6 +14,20 @@ const Index = () => {
           </div>
           <div className='footer-Section'></div>
           <div className='footer-Section'></div>
+          <div className='footer-Section'>
+            <div className='social-icons'>
+              <FaFacebook />
+            </div>
+            <div className='social-icons'>
+              <FaInstagram />
+            </div>
+            <div className='social-icons'>
+              <FaTwitter />
+            </div>
+            <div className='social-icons'>
+              <FaYoutube />
+            </div>
+          </div>
         </div>
         <div className='copyright'>
           <p>&copy; DIGITALAB. All rights reserved. 2021 |</p>
