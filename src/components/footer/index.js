b075bc50c31406dae68074b1627b91e724386d10
Laza993 <laza.bogibilder223@gmail.com>
2021-04-03 21:52:07 +0200

    footer started

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,23 @@
+import './footer.css'
+import { Link } from 'react-router-dom'
+
 const Index = () => {
   return (
     <>
-      <footer id='copyright'>
-        <p>&copy; DIGITALAB. All rights reserved. 2021 |</p>
-      </footer>{' '}
+      <footer className='footer'>
+        <div className='footer-content'>
+          <div className='footer-Section'>
+            <div>
+              <Link to='/'>DigitaLab</Link>
+            </div>
+          </div>
+          <div className='footer-Section'></div>
+          <div className='footer-Section'></div>
+        </div>
+        <div className='copyright'>
+          <p>&copy; DIGITALAB. All rights reserved. 2021 |</p>
+        </div>
+      </footer>
     </>
   )
 }
