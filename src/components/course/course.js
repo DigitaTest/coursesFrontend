b2322b2aa5cd82f28a8fc3ed3c7914d8df9384d9
starac7 <nikolas9997@gmail.com>
2021-04-04 21:42:33 +0200

    refactor

--- src/components/course/course.js
+++ src/components/course/course.js
@@ -6,14 +6,15 @@ import VidPlayer from "./video";
 const Course = () => {
   return (
     <>
+      {/* podaci o kursu, potrebno dodati potrebna predznanja, ''kurs je idealan za ..'' itd. */}
+      <BasicInfo />
       <div onContextMenu={(e) => e.preventDefault()}>
         {/* promo spot, free epizoda  */}
         <VidPlayer />
       </div>
-      {/* podaci o kursu, potrebno dodati potrebna predznanja, ''kurs je idealan za ..'' itd. */}
-      <BasicInfo />
       {/* Ovde dodati listu sadrzaja kursa */}
       {/* Iskustva polaznika */}
+      {/* Opcija placanja ofc */}
       <div className="courseInfo">
         <Link to="/courses" className="button">
           Nazad
